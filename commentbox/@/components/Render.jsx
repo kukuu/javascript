a0825720926de75f1325d7af4d531c0870bac738
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2017-02-01 18:29:03 +0000

    Major house cleanup folder structure

--- commentbox/@/components/Render.jsx
+++
@@ -1,4 +0,0 @@
- ReactDOM.render(
-          React.createElement(CommentBox, null),
-          document.getElementById('content')
-  );
--- snippets/using-props.jsx
+++
@@ -1,9 +0,0 @@
-var MyComponent = React.createClass({
-    render: function(){
-        return (
-            <h1>Hello, {this.props.name}!</h1>
-        );
-    }
-});
-
-React.render(<MyComponent name="Handsome" />, document.getElementById('myDiv'));
