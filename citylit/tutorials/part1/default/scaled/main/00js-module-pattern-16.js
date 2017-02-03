0c0f5edfc040176b37211723f8af3532718118f4
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2017-02-03 14:21:51 +0000

    shopping basket implemented using module patterns

--- citylit/tutorials/part1/default/scaled/main/00js-module-pattern-16.js
+++ citylit/tutorials/part1/default/scaled/main/00js-module-pattern-16.js
@@ -5,7 +5,7 @@
 
 //In JavaScript, the Module pattern is used to further emulate the concept of classes
 // in such a way that we're able to include both public/private methods and variables 
-//inside a single object, thus shielding particular parts from the global scope. 
+//inside a single object, thus shielding particular parts from the global scope and global polution. 
 //What this results in is a reduction in the likelihood of our
 // function names conflicting with other functions defined in additional scripts on the page.
 
